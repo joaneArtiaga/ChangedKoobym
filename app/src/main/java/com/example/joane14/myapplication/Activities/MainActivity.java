1e31cc7dcc8fc1ef0b2b1cfcfd13255a4626d6f0
kimberlycanedo12 <kimberycanedo02@gmail.com>
2017-09-22 23:47:40 +0800

    new login

--- app/src/main/java/com/example/joane14/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/joane14/myapplication/Activities/MainActivity.java
@@ -210,7 +210,7 @@ public class    MainActivity extends AppCompatActivity {
 
     public void login(View view) {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String URL = "http://192.168.1.8:8080/Koobym/user/login";
+        String URL = "http://192.168.1.2:8080/Koobym/user/login";
 //        String URL = Constants.WEB_SERVICE_URL +"user/login";
         User user = new User();
         user.setUsername(mUsername);
