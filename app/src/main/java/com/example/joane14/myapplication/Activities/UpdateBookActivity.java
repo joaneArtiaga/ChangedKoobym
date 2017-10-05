723590a9554f1907749aff6e4e2ca00000261cc5
kimberlycanedo12 <kimberycanedo02@gmail.com>
2017-10-06 06:44:34 +0800

    updated

---
+++ app/src/main/java/com/example/joane14/myapplication/Activities/UpdateBookActivity.java
@@ -0,0 +1,19 @@
+package com.example.joane14.myapplication.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.joane14.myapplication.R;
+
+/**
+ * Created by Kimberly Cañedo on 06/10/2017.
+ */
+
+public class UpdateBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_book);
+    }
+}
