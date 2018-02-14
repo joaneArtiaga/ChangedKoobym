6750f941f6d7323dac80cb6cc080df6e68efcc7a
unknown <joane.artiaga@gmail.com>
2018-02-14 19:07:28 +0800

    removed tablayout sa request activity

--- app/src/main/java/com/example/joane14/myapplication/Activities/RequestActivity.java
+++ app/src/main/java/com/example/joane14/myapplication/Activities/RequestActivity.java
@@ -79,12 +79,6 @@ public class RequestActivity extends AppCompatActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -107,12 +101,6 @@ public class RequestActivity extends AppCompatActivity
 
     }
 
-        private void setupViewPager(ViewPager viewPager) {
-            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-            adapter.addFragment(new RequestFrag(), "Rent");
-            adapter.addFragment(new SwapRequestFrag(), "Swap");
-            viewPager.setAdapter(adapter);
-        }
 
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -211,34 +199,5 @@ public class RequestActivity extends AppCompatActivity
     @Override
     public void ontToApproveOnClick(Uri uri) {
 
-    }
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
     }
 }
